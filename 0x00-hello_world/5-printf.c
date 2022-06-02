9850febb64cd6410b2275dc851562cf948edd8ba
Totweva <totweva@hmail.com>
2022-06-02 05:24:06 -0700

    C lauguage hello world

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main fuction
+ *
+ * printf strings
+ *
+ * return 0
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return(0);
+}
