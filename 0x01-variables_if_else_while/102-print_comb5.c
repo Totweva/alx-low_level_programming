49ac445e1a7291710321b725a371524f16407c41
Totweva <totweva@hmail.com>
2022-06-03 08:23:22 -0700

    programming in C

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,14 +10,14 @@ int main(void)
 {
 	int twodigi1, twodigi2;
 
-	for (twodigi1 = 0; twodigi1 < 98; twodigi1++)
+	for (twodigi1 = 0; twodigi1 <= 98; twodigi1++)
 	{
-		for (twodigi2 = twodigi1 + 1; twodigi2 < 99; twodigi2++)
+		for (twodigi2 = twodigi1 + 1; twodigi2 <= 99; twodigi2++)
 		{
-			putchar((twodigi1 % 10) + '0');
+			putchar((twodigi1 / 10) + '0');
 			putchar((twodigi1 % 10) + '0');
 			putchar(' ');
-			putchar((twodigi2 % 10) + '0');
+			putchar((twodigi2 / 10) + '0');
 			putchar((twodigi2 % 10) + '0');
 
 			if (twodigi1 == 98 && twodigi2 == 99)
